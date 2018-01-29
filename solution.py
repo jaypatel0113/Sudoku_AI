449bdf6af80be853769468d2a7971099a285fe6b
Jay Patel <jay.patel0113@gmail.com>
2018-01-28 20:09:17 -0500

    Added naked twin and diagnolunits

--- solution.py
+++ solution.py
@@ -6,10 +6,12 @@ import itertools
 row_units = [cross(r, cols) for r in rows]
 column_units = [cross(rows, c) for c in cols]
 square_units = [cross(rs, cs) for rs in ('ABC','DEF','GHI') for cs in ('123','456','789')]
-unitlist = row_units + column_units + square_units
+
 
 # TODO: Update the unit list to add the new diagonal units
-unitlist = unitlist
+d1 = [s+t for s,t in zip(rows,cols)]
+d2 = [s+t for s,t in zip(rows,cols[::-1])]
+unitlist = row_units + column_units + square_units + [d1,d2]
 
 units = dict((s, [u for u in unitlist if s in u]) for s in boxes)
 peers = dict((s, set(sum(units[s],[]))-set([s])) for s in boxes)
